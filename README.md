f05ce735ca9680dd76a011838800860c194d08fa
shanissewilson <105824675+shanissewilson@users.noreply.github.com>
2022-06-22 18:53:52 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# Shanisse's Coding Quiz
+
+*Credits*
+
+Created with CodingNepal Original URL: [https://www.codingnepalweb.com/quiz-app-with-timer-javascript/](https://www.codingnepalweb.com/quiz-app-with-timer-javascript/).
+
---
+++ questions.js
@@ -0,0 +1,59 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is Javascript?",
+    answer: "A scripting language program that works as an algorithm",
+    options: [
+      "A cup of coffee",
+      "A scripting language program that works as an algorithm",
+      "A standardized system for tagging files",
+      "A web design program"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which is NOT a Javascript variable?",
+    answer: "Boolean",
+    options: [
+      "Numbers",
+      "Strings",
+      "Boolean",
+      "Functions"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What must string values be enclosed with?",
+    answer: "quotations",
+    options: [
+      "quotations",
+      "commas",
+      "parenthesis",
+      "curly brackets"
+    ]
+  },
+  
+];
